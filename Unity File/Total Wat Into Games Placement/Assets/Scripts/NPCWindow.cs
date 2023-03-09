030557afa4158536721425132aaa0e0ab665389b
DylanJerwood <104373836+DylanJerwood@users.noreply.github.com>
2023-03-09 17:01:04 +0000

    Continued development on karma system

--- Unity File/Total Wat Into Games Placement/Assets/Scripts/NPCWindow.cs
+++ Unity File/Total Wat Into Games Placement/Assets/Scripts/NPCWindow.cs
@@ -11,6 +11,8 @@ public class NPCWindow : MonoBehaviour
     public InteractionManager interactionManager;
     public Karma karmaScript;
     public TMP_Text txt;
+    public Button giveAid;
+    public Button requestAid;
 
     private void Update() {
         if(interactionManager.NPCInteracted == true) {
@@ -22,21 +24,39 @@ public class NPCWindow : MonoBehaviour
         if(Input.GetMouseButtonDown(1)) {
             player.GetComponent<Movement>().enabled = true;
         }
+
+        if(karmaScript.frenchKarmaNum < 70) {
+            giveAid.GetComponent<Button>().enabled = false;
+        }
+        else {
+            giveAid.GetComponent<Button>().enabled = true;
+        }
+
+        if(karmaScript.frenchKarmaNum < 150) {
+            requestAid.GetComponent<Button>().enabled = false;
+        }
+        else {
+            requestAid.GetComponent<Button>().enabled = true;
+        }
+
     }
 
     public void Option1()
     {
-        karmaScript.germanKarmaNum = karmaScript.germanKarmaNum + 10;
-        karmaScript.frenchKarmaNum = karmaScript.frenchKarmaNum - 10;
-        Debug.Log("YOU PICKED OPTION 1");
+        karmaScript.frenchKarmaNum = karmaScript.frenchKarmaNum + 10;
+        Debug.Log("YOU LOST 20 MILITARY SIZE");
         NPCInteractionWindow.GetComponent<Canvas>().enabled = false;
         player.GetComponent<Movement>().enabled = true;
     }
 
     public void Option2() {
-        karmaScript.frenchKarmaNum = karmaScript.frenchKarmaNum + 10;
-        karmaScript.germanKarmaNum = karmaScript.germanKarmaNum - 10;
-        Debug.Log("YOU PICKED OPTION 2");
+        karmaScript.frenchKarmaNum = karmaScript.frenchKarmaNum - 20;
+        Debug.Log("YOU GAINED 60 MILITARY SIZE");
+        NPCInteractionWindow.GetComponent<Canvas>().enabled = false;
+        player.GetComponent<Movement>().enabled = true;
+    }
+
+    public void Back() {
         NPCInteractionWindow.GetComponent<Canvas>().enabled = false;
         player.GetComponent<Movement>().enabled = true;
     }
